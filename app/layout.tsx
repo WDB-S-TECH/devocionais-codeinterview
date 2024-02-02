9383fea223fa7e4116c4e7a1cc7f51537adcd1dd
wneves <drwilliamneves@gmail.com>
2024-02-01 23:45:00 -0500

    Add @vercel/speed-insights package

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./globals.css"
 import { cn } from "@/lib/utils"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
 			<body className={cn(inter.className)}>
 				{children}
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	)
