e1b9a5392314200b388aa5c232fa253f4244ba02
wneves <drwilliamneves@gmail.com>
2024-02-08 19:42:28 -0500

    Refactor musicas page layout

--- app/(site)/musicas/page.tsx
+++ app/(site)/musicas/page.tsx
@@ -11,13 +11,13 @@ export const metadata: Metadata = {
 export default async function MusicaSlug() {
 	const musicas = await runQuery(musicasQuery)
 	return (
-		<>
+		<div className="px-4 py-2 sm:px-6 md:px-4">
 			{musicas.map((musica, i, a) => (
 				<div key={musica.slug}>
 					<MusicaBlockComponent musica={musica} />
 					{a.length !== 1 && i !== a.length - 1 && <div className="divider" />}
 				</div>
 			))}
-		</>
+		</div>
 	)
 }
