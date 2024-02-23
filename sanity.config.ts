1f576924326e53547d540e803d31eae703bcbb4b
wneves <drwilliamneves@gmail.com>
2024-02-22 22:56:43 -0500

    Add @sanity/assist and groqd-playground dependencies

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,11 +5,13 @@
 import { visionTool } from "@sanity/vision"
 import { defineConfig } from "sanity"
 import { structureTool } from "sanity/structure"
+import { assist } from "@sanity/assist"
 import { ptPTLocale } from "@sanity/locale-pt-pt"
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from "./sanity/env"
 import { schema } from "./sanity/schema"
+import { groqdPlaygroundTool } from "groqd-playground"
 
 export default defineConfig({
 	basePath: "/studio",
@@ -20,6 +22,8 @@ export default defineConfig({
 	plugins: [
 		structureTool(),
 		ptPTLocale(),
+		assist(),
+		groqdPlaygroundTool(),
 		// Vision is a tool that lets you query your content with GROQ in the studio
 		// https://www.sanity.io/docs/the-vision-plugin
 		visionTool({ defaultApiVersion: apiVersion }),
